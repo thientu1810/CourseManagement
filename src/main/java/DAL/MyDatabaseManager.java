5dd8889da26f43ae14a510fe60eacf0aceca2888
tungungoc111 <nguyenthanhthientu18nt@gmail.com>
2024-02-20 13:29:28 +0700

    first commit

---
+++ src/main/java/DAL/MyDatabaseManager.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author USER
+ */
+public class MyDatabaseManager {
+    private static Connection c;
+    private static Statement s;
+    private static String host, port, dbName, dbUser, dbPassword;
+    
+    public static void connectDB(){
+        host = "localhost";
+        port = "3306";
+        dbUser = "";
+        dbPassword = "";
+        String dbPath = "jdbc:mysql://" + host + ":" + port + "/"
+                + dbName + "?useUnicode=yes&characterEncoding=UTF-8";
+        try {
+            c = (Connection) DriverManager.getConnection(dbPath, dbUser, dbPassword);
+            s = (Statement) c.createStatement();
+            
+            System.out.println("Connected");
+        } catch (SQLException ex) {
+            System.out.print(ex.getMessage());
+        }
+    }
+    public static Connection getConnection()
+    {
+        return c;
+    }
+    //run sql
+    public static ResultSet doReadQuery(String sql) {
+        ResultSet rs = null;
+        
+        try {
+            rs = s.executeQuery(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(MyDatabaseManager.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
+    //test connection
+    public static void main(String[] args) {
+        MyDatabaseManager.connectDB();
+        
+    }
+}
